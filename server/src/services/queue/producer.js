71aef3a071611c394ae90c69e4e6214ac13667c9
Priti Poddar <pritipoddar461@gmail.com>
2026-07-21 22:47:55 +0530

    redis problem

--- server/src/services/queue/producer.js
+++ server/src/services/queue/producer.js
@@ -2,5 +2,10 @@ import redisClient from "../../config/redis.js";
 import { REDIS_QUEUE } from "../../utils/constants.js";
 
 export const addJobToQueue = async (job) => {
-  await redisClient.lPush(REDIS_QUEUE, JSON.stringify(job));
+  console.log("Queue:", REDIS_QUEUE);
+  console.log("Job:", job);
+
+  const length = await redisClient.lPush(REDIS_QUEUE, JSON.stringify(job));
+
+  console.log("LPUSH successful. Queue length:", length);
 };
